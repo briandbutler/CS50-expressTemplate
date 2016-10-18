a5794156cd91d385ce2d49ae9ab358c4a282b10f
Dylan Tackoor <mynameisdylantackoor@gmail.com>
2016-10-18 02:26:47 -0400

    added login page

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET login page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('login', { title: 'Login page',
+                          css: 'login'});
 });
 
 module.exports = router;
