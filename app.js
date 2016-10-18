fa31fe9280ef956f8e8ec87a7876dfe486516515
Dylan Tackoor <mynameisdylantackoor@gmail.com>
2016-10-18 02:34:26 -0400

    custom 404 error page

--- app.js
+++ app.js
@@ -26,9 +26,8 @@ app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  res.render('404', { title: '404 | Page not found',
+                          css: '404'});
 });
 
 // error handlers
